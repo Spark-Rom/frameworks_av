7e018eeccfa8a6626eaa6e0127b85da51209f3b7
vjspranav <pranavasri@live.in>
2022-12-08 14:26:13 +0000

    Revert "CameraService: Updated watchdog disconnect timer"
    This reverts commit 6dbbb0bff1d0f29e65ed3e547279770f5cc59b1b.

--- services/camera/libcameraservice/common/Camera2ClientBase.cpp
+++ services/camera/libcameraservice/common/Camera2ClientBase.cpp
@@ -41,6 +41,8 @@
 
 namespace android {
 
+const static size_t kDisconnectTimeoutMs = 2500;
+
 using namespace camera2;
 
 // Interface used by CameraService
@@ -250,16 +252,10 @@ status_t Camera2ClientBase<TClientBase>::dumpDevice(
 
 template <typename TClientBase>
 binder::Status Camera2ClientBase<TClientBase>::disconnect() {
-    if (mCameraServiceWatchdog != nullptr && mDevice != nullptr) {
-        // Timer for the disconnect call should be greater than getExpectedInFlightDuration
-        // since this duration is used to error handle methods in the disconnect sequence
-        // thus allowing existing error handling methods to execute first
-        uint64_t maxExpectedDuration =
-                ns2ms(mDevice->getExpectedInFlightDuration() + kBufferTimeDisconnectNs);
-
+    if (mCameraServiceWatchdog != nullptr) {
         // Initialization from hal succeeded, time disconnect.
         return mCameraServiceWatchdog->WATCH_CUSTOM_TIMER(disconnectImpl(),
-                maxExpectedDuration / kCycleLengthMs, kCycleLengthMs);
+                kDisconnectTimeoutMs / kCycleLengthMs, kCycleLengthMs);
     }
     return disconnectImpl();
 }
